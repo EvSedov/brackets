2fa4711ec668a0ace3aaa4f42e197e6631589d99
Evgeniy Sedov <sedoff-online@yandex.ru>
2022-02-10 23:02:11 +0700

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length % 2 !== 0) {
+    return false;
+  }
+  let result = [];
+  for(const bracket of str) {
+    let currentElement;
+    bracketsConfig.forEach((element) => {
+      if (element.includes(bracket)) {
+        currentElement = element;
+      }
+    });
+    if(result[result.length -1] == currentElement[0] && currentElement.lastIndexOf(bracket) == 1) {
+      result.pop();
+      continue;
+    }
+    if(currentElement.indexOf(bracket) == 0) {
+      result.push(bracket);
+    }
+  }
+  return result.length == 0;
+};
